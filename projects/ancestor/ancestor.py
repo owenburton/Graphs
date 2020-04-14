84f3294029ab7adbce1ad2929bbc2da18edb671b
owenburton <owenburton@users.noreply.github.com>
2020-04-13 21:08:36 -0700

    scaffolding of ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,32 @@
+from collections import deque
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # reverse the tuples direction of edges for ancestors
+    lis = [tup[::-1] for tup in ancestors]
+    # create the directed graph
+    graph = {}
+    for tup in lis:
+        if tup[0] in graph:
+            graph[tup[0]].add(tup[1])
+        else:
+            graph[tup[0]] = {tup[1]}
+        if tup[1] in graph:
+            continue
+        else:
+            graph[tup[1]] = {}
+    print(graph)
+    # then do a depth first traversal for the starting node
+    stack = deque([starting_node])
+    visited = set()
+    while len(stack)>0:
+        v = stack.pop()
+        if v not in visited:
+            visited.add(v)
+            print(graph[v])
+            for n in graph[v]:
+                stack.append(n)
+    # return the furthest node
+
+
+test = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+earliest_ancestor(test, 1)
