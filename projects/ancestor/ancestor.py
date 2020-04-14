99cd13e9cdc61695f569f1e8f265446640ce12b0
owenburton <owenburton@users.noreply.github.com>
2020-04-14 08:10:37 -0700

    graph working, dfs working, need to keep track of depth

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -2,6 +2,7 @@ from collections import deque
 
 def earliest_ancestor(ancestors, starting_node):
     # reverse the tuples direction of edges for ancestors
+    # so you can traverse only to older generations
     lis = [tup[::-1] for tup in ancestors]
     # create the directed graph
     graph = {}
@@ -13,20 +14,21 @@ def earliest_ancestor(ancestors, starting_node):
         if tup[1] in graph:
             continue
         else:
-            graph[tup[1]] = {}
-    print(graph)
-    # then do a depth first traversal for the starting node
+            graph[tup[1]] = set()
+    # do a depth first traversal for the starting node
     stack = deque([starting_node])
     visited = set()
     while len(stack)>0:
         v = stack.pop()
         if v not in visited:
             visited.add(v)
-            print(graph[v])
+            # return the furthest node
+            if not graph[v]:
+                print(graph[v])
+                print(v)
             for n in graph[v]:
                 stack.append(n)
-    # return the furthest node
 
 
 test = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-earliest_ancestor(test, 1)
+earliest_ancestor(test, 3)
